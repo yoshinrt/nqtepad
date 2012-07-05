c5c7367855c3edea381a3a8aba524964160c4991
Yoshi NRT <32790691+Yoshi NRT@users.noreply.github.com>
2012-07-05 10:30:25 +0000

    doc 自動オープン機能削除
    git-svn-id: file:///home/yoshi/.svnrepos/nqtepad/trunk@3 f3684fea-19c2-4d1f-9bbf-92d326c5af63

--- nqtepad.c
+++ nqtepad.c
@@ -9,7 +9,7 @@
 2000.04.01	word を ShellExecute すると死ぬので，直接 winword を実行
 2000.06.15	winword の存在を自動認識
 2000.08.20	Win2000 のバージョンチェック対策に，本家の Ver 情報をコピー
-2012.07.04	64bit 版 www
+2012.07.05	64bit バージョンw / doc オープン機能削除
 
 *****************************************************************************/
 
@@ -18,18 +18,26 @@
 #include <stdlib.h>
 #include <tchar.h>
 
-#ifdef _WIN64
-	#define CMD_TXT		_T( "C:\\Program Files (x86)\\WZ EDITOR\\wzeditor.exe" )
-#else
-	#define CMD_TXT		_T( "C:\\Program Files\\WZ Editor\\wzeditor.exe" )
-#endif
+#define CMD_TXT		_T( "C:\\Program Files\\WZ Editor\\wzeditor.exe" )
+#define CMD_TXT32	_T( "C:\\Program Files\\WZ Editor (x86)\\wzeditor.exe" )
 
-#define CMD_DOC		_T( "C:\\Program Files\\Accessories\\WORDPAD.EXE" )
-#define CMD_WORD	_T( "C:\\Program Files\\Microsoft Office\\Office\\WINWORD.EXE" )
-
-const WCHAR	szWordID[] = L"WordDocument";
+#define strchr		_tcsrchr
+#define strcpy_s	_tcscpy_s
+#define sprintf_s	_stprintf_s
+#define fopen		_tfopen
 
 /****************************************************************************/
+
+int FileExist( LPTSTR szFileName ){
+	FILE *fp = fopen( szFileName, _T( "r" ));
+	
+	if( fp ){
+		fclose( fp );
+		return 1;
+	}
+	
+	return 0;
+}
 
 int WINAPI _tWinMain(
 	HINSTANCE	hInst,
@@ -37,75 +45,26 @@ int WINAPI _tWinMain(
 	LPTSTR		lpCmdStr,
 	int			iCmdShow ){
 	
-	TCHAR	szFileName[ MAX_PATH ],
-			szFileNameQuated[ MAX_PATH ],
-			szExecDocFile[ MAX_PATH ];
+	TCHAR	szFileName[ MAX_PATH ];
+	TCHAR	*p;
 	
-	TCHAR	*pBuf, *p;
-	FILE	*fp;
-	UINT	uFileSize, u;
-	BOOL	bDoc = FALSE;
-	
-	do{
-		/* make up file name */
-		
-		if( *lpCmdStr == _T( '\0' )){
-			/* param is NULL */
-			*szFileNameQuated = _T( '\0' );
-			break;
-			
-		}else if(( p = _tcschr( lpCmdStr, _T( '"' )))== NULL ){
-			/* file name is not quated. */
-			_tcscpy( szFileName, lpCmdStr );
-			
-		}else{
-			/* file name is quated. */
-			_tcscpy( szFileName, ++p );
-			if(( p = _tcschr( szFileName, _T( '"' )))!= NULL ) *p = _T( '\0' );
-		}
-		
-		_stprintf( szFileNameQuated, _T( "\"%s\"" ), szFileName );
-		
-		/* is word file? */
-		
-		if(( fp = _tfopen( szFileName, _T( "rb" )))== NULL ) break;
-		
-		/* get file size & alloc memory */
-		fseek( fp, 0, SEEK_END );
-		uFileSize = ftell( fp );
-		fseek( fp, 0, SEEK_SET );
-		
-		if(( pBuf =( TCHAR *)malloc( uFileSize ))== NULL ) break;
-		
-		/* read file */
-		fread( pBuf, sizeof( TCHAR ), uFileSize, fp );
-		fclose( fp );
-		
-		if( uFileSize <= sizeof( szWordID )) break;
-		
-		/* search Word ID */
-		for( u = 0; u <= uFileSize - sizeof( szWordID ); ++u ){
-			if( pBuf[ u ]== *szWordID &&
-				!memcmp( &pBuf[ u ], szWordID, sizeof( szWordID )))
-				break;
-		}
-		
-		free( pBuf );
-		if( u > uFileSize - sizeof( szWordID )) break;
-		
-		/* open the word file */
-		bDoc = TRUE;
-		
-		// is winword exist?
-		if(( UINT )FindExecutable( CMD_WORD, NULL, szExecDocFile ) <= 32 )
-			_tcscpy( szExecDocFile, CMD_DOC );
-		
-	}while( 0 );
+	if(
+		*lpCmdStr != _T( '\0' ) &&
+		( p = strchr( lpCmdStr, _T( '"' ))) == NULL
+	){
+		/* file name is not quated. */
+		sprintf_s( szFileName, MAX_PATH, _T( "\"%s\"" ), lpCmdStr );
+		p = szFileName;
+	}else{
+		p = lpCmdStr;
+	}
 	
 	/* open the text file */
-	
-	ShellExecute( NULL, _T( "open" ), bDoc ? szExecDocFile : CMD_TXT,
-		szFileNameQuated, NULL, iCmdShow );
+	ShellExecute(
+		NULL, _T( "open" ),
+		FileExist( CMD_TXT32 ) ? CMD_TXT32 : CMD_TXT,
+		p, NULL, iCmdShow
+	);
 	
 	return( 0 );
 }
