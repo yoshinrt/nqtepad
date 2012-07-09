8dcb3f42ff5c93360edbd9b55b69ed88d5a425d9
Yoshi NRT <32790691+Yoshi NRT@users.noreply.github.com>
2012-07-09 14:44:07 +0000

    引用符処理を削ったのがまずかったので復活
    git-svn-id: file:///home/yoshi/.svnrepos/nqtepad/trunk@6 f3684fea-19c2-4d1f-9bbf-92d326c5af63

--- nqtepad.c
+++ nqtepad.c
@@ -21,8 +21,13 @@
 #define CMD_TXT		_T( "C:\\Program Files\\WZ Editor\\wzeditor.exe" )
 #define CMD_TXT32	_T( "C:\\Program Files (x86)\\WZ Editor\\wzeditor.exe" )
 
-#define strchr		_tcsrchr
+#define strchr		_tcschr
+#define strrchr		_tcsrchr
+#define strcpy		_tcscpy
 #define strcpy_s	_tcscpy_s
+#define strncpy_s	_tcsncpy_s
+#define strcat_s	_tcscat_s
+#define strlen		_tcslen
 #define sprintf_s	_stprintf_s
 #define fopen		_tfopen
 
@@ -45,11 +50,38 @@ int WINAPI _tWinMain(
 	LPTSTR		lpCmdStr,
 	int			iCmdShow ){
 	
+	TCHAR	szFileName[ MAX_PATH ];
+	TCHAR	*pSt, *pEd;
+	TCHAR	*szParam = lpCmdStr;
+	
+	// lpCmtStr が空文字でなく，"～" で囲まれていない場合，囲む
+	if( lpCmdStr && strchr( lpCmdStr, _T( '"' )) == NULL ){
+		
+		// 先頭の空白以外文字をサーチ
+		for( pSt = lpCmdStr; *pSt && *pSt < _T( ' ' ); ++ pSt );
+		
+		// 後端の空白以外文字をサーチ
+		for(
+			pEd = lpCmdStr + strlen( lpCmdStr ) - 1;
+			pEd >= lpCmdStr && *pEd < _T( ' ' );
+			--pEd
+		);
+		++pEd;
+		
+		if( pSt < pEd ){
+			szFileName[ 0 ] = _T( '"' );
+			strncpy_s( szFileName + 1, MAX_PATH - 1, pSt, pEd - pSt );
+			strcpy( szFileName + ( pEd - pSt ) + 1, _T( "\"" ));
+			
+			szParam = szFileName;
+		}
+	}
+	
 	/* open the text file */
 	ShellExecute(
 		NULL, _T( "open" ),
 		FileExist( CMD_TXT32 ) ? CMD_TXT32 : CMD_TXT,
-		lpCmdStr, NULL, iCmdShow
+		szParam, NULL, iCmdShow
 	);
 	
 	return( 0 );
