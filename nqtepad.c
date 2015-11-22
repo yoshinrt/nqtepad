2ad5b1dd3422580e1a63f9ebf57c34b014abe575
Yoshi NRT <32790691+Yoshi NRT@users.noreply.github.com>
2015-11-22 12:08:20 +0000

    VC2015 に変更 xyzzycli に変更，shift キーで xyzzy になる 綺麗なアイコン
    git-svn-id: file:///home/yoshi/.svnrepos/nqtepad/trunk@7 f3684fea-19c2-4d1f-9bbf-92d326c5af63

--- nqtepad.c
+++ nqtepad.c
@@ -2,24 +2,12 @@
 		
 		えせ Notepad
 		
-1999.07.20	Wz macro の IfDoc の機能を移植
-1999.12.16	ごく小さいファイルサイズの .doc を開くと不正終了してた
-1999.12.26	ファイル全体を読みこんで szWordID をサーチ
-1999.12.28	起動パラメータが与えられなかったときに対応
-2000.04.01	word を ShellExecute すると死ぬので，直接 winword を実行
-2000.06.15	winword の存在を自動認識
-2000.08.20	Win2000 のバージョンチェック対策に，本家の Ver 情報をコピー
-2012.07.05	64bit バージョンw / doc オープン機能削除
-
 *****************************************************************************/
 
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <tchar.h>
-
-#define CMD_TXT		_T( "C:\\Program Files\\WZ Editor\\wzeditor.exe" )
-#define CMD_TXT32	_T( "C:\\Program Files (x86)\\WZ Editor\\wzeditor.exe" )
 
 #define strchr		_tcschr
 #define strrchr		_tcsrchr
@@ -43,6 +31,13 @@ int FileExist( LPTSTR szFileName ){
 	
 	return 0;
 }
+
+const TCHAR *g_szEditor[] = {
+	_T( "C:\\Program Files\\xyzzy\\xyzzycli.exe" ),
+	_T( "C:\\Program Files\\xyzzy\\xyzzy.exe" ),
+	_T( "C:\\Program Files (x86)\\xyzzy\\xyzzycli.exe" ),
+	_T( "C:\\Program Files (x86)\\xyzzy\\xyzzy.exe" ),
+};
 
 int WINAPI _tWinMain(
 	HINSTANCE	hInst,
@@ -78,9 +73,12 @@ int WINAPI _tWinMain(
 	}
 	
 	/* open the text file */
+	int iCmd = FileExist( g_szEditor[ 0 ]) ? 0 : 2;
+	if( GetKeyState( VK_SHIFT ) & 0x80000000 ) iCmd += 1;
+	
 	ShellExecute(
 		NULL, _T( "open" ),
-		FileExist( CMD_TXT32 ) ? CMD_TXT32 : CMD_TXT,
+		g_szEditor[ iCmd ],
 		szParam, NULL, iCmdShow
 	);
 	
